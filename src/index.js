7b614d37616dc766a34937d6879f98e3fd463d26
Abhishekkp42 <96106223+Abhishekkp42@users.noreply.github.com>
2022-03-21 17:13:59 +0530

    small changes

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import {BookContextFun} from "./context/BookContext"
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
+    <BookContextFun>
     <App />
+    </BookContextFun>
     </BrowserRouter>
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
